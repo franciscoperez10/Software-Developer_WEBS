3a8d7b4f869629968606a69cdcdb85a1e82605b0
franciscoperez10 <fmlperez95@gmail.com>
2025-05-18 15:31:30 +0100

    push to PC

--- js/exs.js
+++ js/exs.js
@@ -1,32 +1,67 @@
-/* const message = "  TASTE THE RAINBOW  ";
+//ex 17
+let myLuckyNumber = 16;
+let myName = 'Sara';
 
-let whisper = message;
 
-whisper.trim(message);
+//sintaxe tradicional 
+console.log("o meu nome é "+myName+ " e o meu número preferido é "+myLuckyNumber);
 
-console.log(whisper.toLowerCase())
+//sintaxe simplificada 
+console.log(`o meu nome é ${myName} e o meu número preferido ${myLuckyNumber}`);
 
+
+//ex pp 28
+const MESSAGE = " TASTE THE RAINBOW ";
+console.log(MESSAGE)
+let whisper = MESSAGE.toLowerCase().trim();
+console.log(whisper)
+
+//pp30
 const WORD = "skateboard";
+let facialHair = WORD.slice(5).replace("o", "e")
+console.log(facialHair);
 
-let facialHair = WORD.slice(5); 
-facialHair = facialHair.replace("o", "e"); // Armazena o valor retornado
-console.log(facialHair); */
+let myVar;
 
+console.log(myVar)
 
-/* let primeiroNome = prompt ("Qual o teu 1º nome?");
-let segundoNome = prompt ("Qual o teu 2º nome?");
+//ex pp 35
+// let myFirstVar = prompt('Qual é o teu primeiro nome?');
+// let surname = prompt('Qual é o teu primeiro apelido?');
 
-/* let nomeDaPessoa = primeiroNome + " " + segundoNome;
-alert( "Olá, " + primeiroNome + " " + segundoNome); */
+// alert(`Olá ${myFirstVar} ${surname}`);
+// console.log(`Olá ${myFirstVar} ${surname}`);
 
-/* let diaDaSemana = prompt ("Qual o dia da semana?");
-if (diaDaSemana.toLowerCase() == "sexta") {
-    alert("Yay, sobrevivemos a mais uma semana!");
-} else (alert("Boa sorte!")); */
+//ex pp 39
+// let userWeekDay = prompt('que dia da semana é?');
 
-/*let password = prompt("Qual a password?");
-if (password.length > 6) {
-    alert("Acesso permitido!");  
-} else (alert("Acesso negado!")); */
+// if(userWeekDay.toLowerCase().trim() == 'sexta'){
+//     console.log('yay, sobrevivemos a mais uma semana');
+// }else{
+//     console.log('força nisso');
+// }
 
+// let userPass = prompt('Qual é a sua pass?');
 
+// if(userPass.length < 6){
+//     console.log('a pass tem que ter pelo menos 6 caracteres');
+// }else{
+//     console.log('pass ok');
+// }
+
+//pp 43
+// let planets = ['Mercúrio', 'Vénus', 'Terra', 'Mart', 'Jupiter', 'Saturno', 'Urano', 'Neptuno', 'Plutão'];
+// let oldPlanets = planets;
+// planets[3] = 'Marte';
+// planets.pop();
+// planets.unshift('novoPlaneta');
+// console.log(planets)
+
+//pp 55
+for(let i =0; i<6; i++){
+    console.log(i+ ' Da ba dee da ba daa');
+}
+
+for(let i = 25 ; i>=5 ; i-=5){
+    console.log(i);
+}
--- js/script.js
+++ js/script.js
@@ -1,81 +1,44 @@
-/* let myFirstVar = "Olá Mundo!";
+let myFirstVar = "Olá Mundo!";
 let jsIsCool = true;
 
 console.log(jsIsCool);
 jsIsCool= "aborreci-me dele";
 console.log(jsIsCool);
 jsIsCool= "12";
-console.log(jsIsCool+1); */
+console.log(jsIsCool+1);
 
-/* let day1 = "segunda-feira";
-let day2 = "terça-feira";
-let day3 = "quarta-feira";
+//código que lança uma caixa de texto ao user a questionar e coloca a resposta dentro de uma variável
+// let millionDolarQuestion =  prompt('que tal a semana com feriado e apagão?');
+// console.log(millionDolarQuestion);
+let day1 = 'segunda';
+let day2 = 'terça';
+let day3 = 'quarta';
 
-let weekDays = ['segunda-feira', 'terça-feira', 'quarta-feira'];
-console.log(weekDays[0]);
+//console.log('a segunda posição é ' +day3[2]);
 
-let shoppingList = ['banana', 'maçã', 'laranja'];
-let item = prompt("Qual o item que quer adicionar à lista?");
-newShoppingList.push(item);
-console.log(shoppingList); */
+//arrays
+let weekDays = ['segunda', 'terça', 'quarta'];
+// let studentNotes = [18, 19, 20, 17];
 
-/* let planets = ['Mercúrio', 'Vénus', 'Terra', 'Mart', 'Júpiter', 'Saturno', 'Urano', 'Neptuno', 'Plutão'];
-planets [3] = planets[3].replace('Mart', 'Marte');
-console.log(planets[3]);
+// let ourSuperArr = ['segunda', true, 'Joana', 17];
 
-planets.pop();
-console.log(planets);
+console.log('o array na posição 1 tem ' + weekDays[1][4].toUpperCase());
 
-planets.unshift('Galaxy');
-console.log(planets); */
+//ambos têm dentro o A
+//weekDays[1][4]
+//let myDay = weekDays[1];
+//console.log(myDay[4])
 
+//métodos de arrays
+let shoppingList = ['batatas', 'feijão', 'bananas', 'uvas'];
+//let item = prompt('Qual é o item a adicionar');
 
-/* for (let i = 1; i < 7; i++) {
-    console.log("Da ba dee da ba da");
+//shoppingList.push(item)
+
+// console.log(shoppingList)
+// console.log(shoppingList[0]);
+// console.log(shoppingList[1]);
+
+for(let i = 0; i<shoppingList.length; i++){
+    console.log('compra ' +shoppingList[i]);
 }
-
-for (let i = 25; i>= 5; i -= 5) {
-    console.log(i);
-} */
-
-/* const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
-const uppercasedPeople = [];
-for (let i = 0; i < people.length; i++) {
-    uppercasedPeople.push(people[i].toUpperCase());
-}
-console.log(uppercasedPeople); */
-
-/*EXERCÍCIO 1
-
-const shoppingList = [];
-let adicionarItem = true;
-
-while (adicionarItem) {
-    const item = prompt("Qual o item que quer adicionar à lista? (Digite 'fim' para terminar)");
-    if (item && item.toLowerCase() === "fim") {
-        adicionarItem = false; // Termina o ciclo se o utilizador digitar "fim"
-    } else if (item) {
-        shoppingList.push(item); // Adiciona o item à lista se for válido
-    } else {
-        alert("Por favor, insira um item válido."); // Alerta para entradas inválidas
-    }
-}
-
-console.log("Lista de compras:", shoppingList); */
-
-/*EXERCÍCIO 2*/
-
-const shoppingList = [];
-let adicionarItem = true;
-while (adicionarItem) {
-    const item = prompt("Qual o item que quer adicionar à lista? (Digite 'fim' para terminar)");
-    if (item && item.toLowerCase() === "fim") {
-        adicionarItem = false; 
-    } else if (item) {
-        shoppingList.push(item); 
-    } else {
-        alert("Por favor, insira um item válido."); 
-    }
-}
-
-console.log("Lista de compras:", shoppingList);
